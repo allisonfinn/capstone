57e127f57db1e1b87ee996b4022a2a3b9ea1cb42
Allison Finn <finn.allison@gmail.com>
2022-02-10 13:23:59 -0500

    Capstone 1- add comments

--- 19_20_Capstone/capstone/src/main/java/com/techelevator/VendingMachine.java
+++ 19_20_Capstone/capstone/src/main/java/com/techelevator/VendingMachine.java
@@ -1,4 +1,13 @@
 package com.techelevator;
 
 public class VendingMachine {
+    /* vending machine class holds items
+-map of items-inventory starts with 5
+-displayItems
+-feedMoney (adds to balance)
+-getBalance
+-purchaseItem (decrements inventory amt and customer balance)
+   - get sound
+-if sold out , error message
+*/
 }
